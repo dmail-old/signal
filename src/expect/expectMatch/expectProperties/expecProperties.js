53f4e2ac28f2e289ace1ad2d9c2017dd61633510
Damien Maillard <dmaillard06@gmail.com>
2017-10-06 08:23:02 +0200

    update expectProperties

---
+++ src/expect/expectMatch/expectProperties/expecProperties.js
@@ -0,0 +1,50 @@
+import { expectMatch, createMatcher, createExpectFromMatcher } from "../expectMatch.js"
+import { failed, collect, any, passed } from "../../expect.js"
+import { expectObject, expectFunction } from "../../expectType/expectType.js"
+
+const compareProperties = (actual, expected, { allowExtra = false, allowMissing = false }) =>
+	any([expectObject(actual), expectFunction(actual)]).then(() => {
+		const actualPropertyNames = Object.keys(actual)
+		const expectedPropertyNames = Object.keys(expected)
+		const propertyExpectations = []
+
+		expectedPropertyNames.forEach(name => {
+			if (actualPropertyNames.includes(name)) {
+				propertyExpectations.push(expectMatch(actual[name], expected[name]))
+			} else if (allowMissing === false) {
+				propertyExpectations.push(failed(`missing ${name} property`))
+			}
+		})
+
+		if (allowExtra === false) {
+			actualPropertyNames.forEach(name => {
+				if (expectedPropertyNames.includes(name) === false) {
+					propertyExpectations.push(failed(`unexpected ${name} property`))
+				}
+			})
+		}
+
+		return collect(propertyExpectations)
+	})
+
+const mapObject = (object, fn) => {
+	const mappedObject = {}
+	Object.keys(object).forEach((name, index) => {
+		mappedObject[name] = fn(name, index, object[name], object)
+	})
+	return mappedObject
+}
+const matchAny = () => createMatcher(() => passed())
+
+export const matchProperties = expected =>
+	createMatcher(actual => compareProperties(actual, expected))
+export const matchPropertiesAllowingExtra = expected =>
+	createMatcher(actual => compareProperties(actual, expected, { allowExtra: true }))
+export const matchPropertyNames = expected =>
+	createMatcher(actual => {
+		compareProperties(actual, mapObject(expected, matchAny))
+	})
+export const expectProperties = createExpectFromMatcher(matchProperties)
+export const expectPropertiesAllowingExtra = createExpectFromMatcher(matchPropertiesAllowingExtra)
+export const expectPropertyNames = createExpectFromMatcher(matchPropertyNames)
+// expectPropertyNamesAllowingExtra
--- src/expect/expectMatch/index.js
+++ src/expect/expectMatch/index.js
@@ -1,4 +1,4 @@
 export * from "./expectBetween/expectBetween.js"
 export * from "./expectClose/expectClose.js"
-export * from "./expectMatchProperties/expectMatchProperties.js"
+export * from "./expectProperties/expectProperties.js"
 export * from "./expectMatch.js"
