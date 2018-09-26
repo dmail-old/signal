50dc83b520307b8e0c7e78b6c5f2851055960033
Damien Maillard <damien.maillard@dailymotion.com>
2018-09-26 15:35:01 +0200

    use @dmail/project-structure-compile-babel

--- script/compile.js
+++ script/compile.js
@@ -1,50 +1,9 @@
-const fs = require("fs")
+const { compileRoot } = require("@dmail/project-structure-compile-babel")
 const path = require("path")
-const { createRoot } = require("@dmail/project-structure")
-const { transform, options } = require("@dmail/project-babel")
-const { writeFile } = require("./helper.js")
 
-const getFileContentAsString = (location) =>
-  new Promise((resolve, reject) => {
-    fs.readFile(location, (error, buffer) => {
-      if (error) {
-        reject(error)
-      } else {
-        resolve(buffer.toString())
-      }
-    })
-  })
-
-const compileRoot = (root) => {
-  return createRoot(root).then(({ forEachFileMatching }) => {
-    return forEachFileMatching(
-      ({ build }) => build,
-      ({ absoluteName, relativeName }) => {
-        return getFileContentAsString(absoluteName).then((source) => {
-          return transform(source, options, {
-            filename: absoluteName,
-            sourceMaps: true,
-            sourceFileName: relativeName,
-          }).then(({ code, map }) => {
-            const buildLocation = `${root}/dist/${relativeName}`
-
-            if (map) {
-              const sourceMapLocation = `./${path.basename(relativeName)}.map`
-              code = `${code}
-	//# sourceMappingURL=${sourceMapLocation}`
-              return Promise.all([
-                writeFile(buildLocation, code),
-                writeFile(sourceMapLocation, JSON.stringify(map, null, "  ")),
-              ])
-            }
-
-            return writeFile(buildLocation, code)
-          })
-        })
-      },
-    )
-  })
-}
-
-const root = path.resolve(__dirname, "../")
-compileRoot(root)
+compileRoot({
+  root: path.resolve(__dirname, "../"),
+  into: "dist",
+  name: "node",
+  version: "8.0",
+})
